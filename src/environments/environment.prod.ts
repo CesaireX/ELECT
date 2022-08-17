00498fd3aa34b8f23f359b1692b8ed78a55a0f25
CesaireX <cesairetoe@gmail.com>
2022-08-17 11:22:25 +0000

    first4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCF_Xp1oD12Vs-NfeLxK4MDhaXLvWECJTM",
+    authDomain: "elect-btp.firebaseapp.com",
+    projectId: "elect-btp",
+    storageBucket: "elect-btp.appspot.com",
+    messagingSenderId: "110676919069",
+    appId: "1:110676919069:web:f6602013a0c39c2a04d7f4",
+    measurementId: "G-T87WMJKHYS"
+  },
 };
